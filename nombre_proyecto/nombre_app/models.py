dc7b9f60d08ea201259ef0cfac5b35abbfe13b32
Jose Carlos Pérez Loya <pp_joose@hotmail.com>
2023-06-24 12:26:39 -0600

    verificar todo

--- nombre_proyecto/nombre_app/models.py
+++ nombre_proyecto/nombre_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Productos(models.Model):
+   
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+    fecha_registro = models.DateField(auto_now_add=True)
+    estatus = models.BooleanField(default=True)
+
--- nombre_proyecto/nombre_app/views.py
+++ nombre_proyecto/nombre_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Productos
+from .forms import ProductoForm
 
-# Create your views here.
+def productos(request):
+    productos = Productos.objects.all()
+    return render(request, 'productos.html', {'productos': productos})
+
+def ingresar_producto(request):
+    form = ProductoForm()
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Realizar acciones adicionales si es necesario
+    return render(request, 'ingresar_producto.html', {'form': form})
